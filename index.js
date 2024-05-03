02ab76862cdb626eaad95fa746c41502984b819e
Gabriel <141271554+GabrielPMorgado@users.noreply.github.com>
2024-05-02 21:34:19 -0300

    att

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const host='0.0.0.0';
+const porta = 3000;
+
+
+
+
+
+
+
+app.listen(porta, host,()=>{
+    console.log(`Servidor executando em http://${host}:${porta}.`);
+});
